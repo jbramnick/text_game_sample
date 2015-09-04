93ca101582cc0a772b32e0df7f7cd326d1570ff1
Nathan Woodhead <nwoodhea@mail.umw.edu>
2015-09-03 21:50:56 -0400

    Set up the bones of the Dungeon class.

---
+++ src/edu/umw/nwoodhea/bork/Dungeon.java
@@ -0,0 +1,22 @@
+package edu.umw.nwoodhea.bork;
+public class Dungeon{
+private String name;
+
+public void Dungeon (Room entry, String name){
+	this.name = name;
+	
+}
+
+public Room getEntry(){
+	return null;
+}
+public String getName(){
+	return null;
+}
+public void add(Room room){
+		
+}
+public Room getRoom(String roomTitle){
+	return null;
+}
+}
