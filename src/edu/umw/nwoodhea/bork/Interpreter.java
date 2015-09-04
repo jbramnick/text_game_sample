1c408e6d15c00c5ed80545b8527480e64266e1e6
Nathan Woodhead <nwoodhea@mail.umw.edu>
2015-09-03 20:11:28 -0400

    Finished step 1

--- src/edu/umw/nwoodhea/bork/Interpreter.java
+++ src/edu/umw/nwoodhea/bork/Interpreter.java
@@ -1,7 +1,18 @@
-
 package edu.umw.nwoodhea.bork;
+import java.util.Scanner;
+
 
 class Interpreter {
 
+public static void main (String args[]){
+	String input;
+	Scanner scan = new Scanner(System.in);
+	input = scan.next();
+	while(!input.equals("q")){
+		input = scan.next();
 }
+}
+}
+
+
 
