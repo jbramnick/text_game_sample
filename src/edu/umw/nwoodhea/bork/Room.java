592f30731e617146b4f63cc515ea5b4e3a9de9bc
Nathan Woodhead <nwoodhea@mail.umw.edu>
2015-09-03 20:53:36 -0400

    Constructor, setDesc, describe finished.

--- src/edu/umw/nwoodhea/bork/Room.java
+++ src/edu/umw/nwoodhea/bork/Room.java
@@ -1,8 +1,27 @@
 package edu.umw.nwoodhea.bork;
 
+public class Room{
+
 private String title;
 private String desc;
-private boolean beenHere;
+private boolean beenHere = false;
+public void Room(String title){
+	this.title = title;
 
+}
+public void setDesc(String desc){
+	this.desc = desc;
+}
+String describe(){
+	if(!beenHere){
+		beenHere = !beenHere;
+		return desc;
+}
+	else{
+		return null;
+}
+}
+
+}
 
  
