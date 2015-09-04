383eeb828443342269f29438f3e47de7a9eee307
Nathan Woodhead <nwoodhea@mail.umw.edu>
2015-09-03 22:47:48 -0400

    Wall hit need to ask questions.

--- src/edu/umw/nwoodhea/bork/Dungeon.java
+++ src/edu/umw/nwoodhea/bork/Dungeon.java
@@ -1,10 +1,25 @@
 package edu.umw.nwoodhea.bork;
+import java.util.Hashtable;
 public class Dungeon{
+
 private String name;
 
 public void Dungeon (Room entry, String name){
 	this.name = name;
-	
+	Room one = new Room("Nathan's Room");
+	Room two = new Room("Heather's Room");
+	Room three = new Room("Hallway");
+	Room four = new Room("Kitchen");
+	Room five = new Room("Living Room");
+
+
+	Hashtable map = new Hashtable(5);
+	map.put("1", one);
+	map.put("2", two);
+	map.put("3", three);
+	map.put("4", four);
+	map.put("5", five);
+
 }
 
 public Room getEntry(){
@@ -13,10 +28,11 @@ public Room getEntry(){
 public String getName(){
 	return null;
 }
+//Need to ask about this method.
 public void add(Room room){
-		
+			
 }
 public Room getRoom(String roomTitle){
-	return null;
+	return map.get(roomTitle);
 }
 }
