2f8ae540bd7df60ea001f515050336f381a29c44
Nathan Woodhead <nwoodhea@mail.umw.edu>
2015-09-06 15:37:45 -0400

    Started the framework for Exit.java

---
+++ src/edu/umw/nwoodhea/bork/Exit.java
@@ -0,0 +1,22 @@
+package edu.umw.nwoodhea.bork;
+
+class Exit{
+
+private String dir;
+
+public void Exit(String dir, String src, String dest){
+
+}
+String describe(){
+	String text = "You can go " + dir + " to " + dest + ".";
+	return text;
+}
+public String getDir(){
+	return null;
+}
+
+public Room getLoc(){
+	
+	return null;
+}
+}
