6f927d30fffd95dff9fd2bc0efb14b3013ef6603
Nathan Woodhead <nwoodhea@mail.umw.edu>
2015-09-19 20:57:18 -0400

    Got the file reading done.

--- src/edu/umw/nwoodhea/bork/Dungeon.java
+++ src/edu/umw/nwoodhea/bork/Dungeon.java
@@ -1,18 +1,28 @@
 package edu.umw.nwoodhea.bork;
 import java.util.Hashtable;
 import java.util.Scanner;
-import java.io.FileReader;
+import java.io.*;
 public class Dungeon{
 
 private String name;
 private Hashtable <String,Room> map =  new Hashtable<String,Room>(5);
 private Room entry;
-public Dungeon (String filename) throws exception {
-	FileReader fr = new FileReader(filename);
-	System.out.println(filename);
-	Scanner scanner = new Scanner(filename);
+public Dungeon (String filename){
+//	FileReader fr = new FileReader(filename);
+//	System.out.println(filename);
+	try{
+		Scanner scanner = new Scanner(new File(filename));
+		while(scanner.hasNextLine()){
+			System.out.println(scanner.next());
+}
 }
 
+	catch (FileNotFoundException e)
+        {
+            System.out.println("File not found");
+
+        } 
+}
 public Room getEntry(){
 	return entry;
 }
