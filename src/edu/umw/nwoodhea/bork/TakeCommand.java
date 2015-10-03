2ccc70ee5c1286597a2ec230273dca7167f6325e
Nathan Woodhead <nwoodhea@mail.umw.edu>
2015-10-03 15:31:10 -0400

    Take comand added

---
+++ src/edu/umw/nwoodhea/bork/TakeCommand.java
@@ -0,0 +1,19 @@
+package edu.umw.nwoodhea.bork;
+class TakeCommand extends Command{
+	private String itemName;
+	TakeCommand(String itemName){
+		this.itemName = itemName;
+	}
+	String execute(){
+		Room currentRoom = GameState.instance().getAdventurersCurrentRoom();
+		try{
+			Item item = currentRoom.getItemNamed(itemName);
+			currentRoom.remove(item);
+			GameState.instance().addToInventory(item);
+			return "";
+		}
+		catch(Item.NoItemException e){
+			return "There is no " + itemName + "here.";
+		}
+	}
+}	
