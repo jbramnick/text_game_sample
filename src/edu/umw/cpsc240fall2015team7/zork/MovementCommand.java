db2bf56644a99ae87bd068af10261424cd5611ab
Carson Meadows <cmeadows@rosemary@umw.edu>
2015-11-04 18:04:08 -0500

    Commented for LookCommand and MovementCommand

--- src/edu/umw/cpsc240fall2015team7/zork/MovementCommand.java
+++ src/edu/umw/cpsc240fall2015team7/zork/MovementCommand.java
@@ -1,11 +1,23 @@
+/**
+*Moves the player from Room to Room.
+*@author Carson Meadows
+*/
 package edu.umw.cpsc240fall2015team7.zork;
 class MovementCommand extends Command{
 
 	private String dir;
 
+	/**
+	*Constructs this MovementCommand.
+	*@author Carson Meadows
+	*/
 	MovementCommand(String dir){
 		this.dir = dir;
 	}
+	/**
+	*Moves the player to the adjacent Room in the direction of this.
+	*@author Carson Meadows
+	*/
 	String execute(){
 		Room currentRoom = GameState.instance().getAdventurersCurrentRoom();
 		try{
