200624766766bbd032e867abd191b3fb2ad26f34
Nathan Woodhead <nwoodhea@mail.umw.edu>
2015-11-04 17:36:39 -0500

    Added ScoreEvent and WoundEvent

---
+++ src/edu/umw/cpsc240fall2015team7/zork/ScoreEvent.java
@@ -0,0 +1,11 @@
+/**
+  *Changes the score. Takes an int and changes the players score by that ammount.
+  *@author Nathanael Woodhead
+  */
+package edu.umw.cpsc240fall2015team7.zork;
+class ScoreEvent{
+	private int score;
+
+	ScoreEvent(int score){
+	}
+	String changeScore(
---
+++ src/edu/umw/cpsc240fall2015team7/zork/WoundEvent.java
@@ -0,0 +1,27 @@
+package edu.umw.cpsc240fall2015team7.zork;
+/**
+  *Decreases the players health and checks to see if the player is dead.
+  *@author Nathanael Woodhead
+  */
+class WoundEvent{
+	private int damage;
+	/**
+	  *Creates a new WoundEvent object. 
+	  *@param damage number of hitpoints to remove from player.
+	  *@author Nathanael Woodhead
+	  */
+	WoundEvent(int damage){
+
+	}
+	/**
+	  *Removes damage from the player.
+	  */
+	String removeDamage(){
+
+	}
+	/**
+	  *Checks to see if the player is dead. The player is dead when health falls below 1.
+	  */
+	boolean checkDead(){
+	}
+}
