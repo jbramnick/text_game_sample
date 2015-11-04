c3a86df9ecb39db88f8170d3eafdaa86838ffadc
Carson Meadows <cmeadows@rosemary@umw.edu>
2015-11-04 18:41:03 -0500

    Commented SaveCommand

--- src/edu/umw/cpsc240fall2015team7/zork/SaveCommand.java
+++ src/edu/umw/cpsc240fall2015team7/zork/SaveCommand.java
@@ -1,12 +1,24 @@
+/**
+*Command that saves the current game status
+*@author Carson Meadows
+*/
 package edu.umw.cpsc240fall2015team7.zork;
 class SaveCommand extends Command{
 	private String saveFilename;
 	
+	/**
+	*Constructs this SaveCommand.
+	*@author Carson Meadows
+	*/
 	SaveCommand(String f){
 		this.saveFilename = f;
-		}
+	}
+	/**
+	*Saves current game status. Returns confirmation String.
+	*@author Carson Meadows
+	*/
 	String execute(){
 		GameState.instance().store();
 		return "Game Saved!";
-		}
 	}
+}
