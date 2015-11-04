66b865381b5899a087b0ba0ae1aa04bf2417b0fa
Nathan Woodhead <nwoodhea@mail.umw.edu>
2015-11-04 18:08:42 -0500

    Added WinEvent

---
+++ src/edu/umw/cpsc240fall2015team7/zork/WinEvent.java
@@ -0,0 +1,21 @@
+/**
+  *Ends the game and returns a win message and the score.
+  *@author Nathanael Woodhead
+  */
+package edu.umw.cpsc240fall2015team7.zork;
+class WinEvent{
+	/**
+	  *Creates a new WinEvent object.
+	  *@author Nathanael Woodhead
+	  */
+	WinEvent(){
+	}
+	/**
+	  *Ends the game and returns a win message.
+	  *@author Nathanael Woodhead
+	  */
+	String win(){
+		return ""
+	}
+}
+
