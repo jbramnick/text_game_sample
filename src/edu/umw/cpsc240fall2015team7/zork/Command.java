ad597c75bba7ca062eec73228fa6ef24aede839e
Nathan Woodhead <nwoodhea@mail.umw.edu>
2015-11-04 22:45:53 -0500

    small change

--- src/edu/umw/cpsc240fall2015team7/zork/Command.java
+++ src/edu/umw/cpsc240fall2015team7/zork/Command.java
@@ -1,9 +1,9 @@
-/**
-*Abstract super class for all commands.
-*@author Nathanael Woodhead
-*/
 package edu.umw.cpsc240fall2015team7.zork;
 import java.lang.IllegalArgumentException;
+/**
+ *Abstract super class for all commands.
+ *@author Nathanael Woodhead
+ */
 abstract class Command{
 	/**
 	*super method for all subclasses to overwrite regarding their command behavior once executed.
