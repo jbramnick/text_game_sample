7fa783c62c323c21029feeb2e02fb79a11f72cd3
Nathan Woodhead <nwoodhea@mail.umw.edu>
2015-11-04 22:28:59 -0500

    Added the Event superclass

---
+++ src/edu/umw/cpsc240fall2015team7/zork/Event.java
@@ -0,0 +1,12 @@
+/**
+  *Abstract superclass of all Events.
+  *@author Nathanael Woodhead
+  */
+package edu.umw.cpsc240fall2015team7.zork;
+abstract class Event{
+	/**
+	  *Super method for all Events to overwrite regarding that events behaivor.
+	  *@author Nathanael Woodhead
+	  */
+	abstract String execute(){}
+}
