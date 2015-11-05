0d5018b4725937ada5f4e613389a7207db251ec3
Nathan Woodhead <nwoodhea@mail.umw.edu>
2015-11-04 22:24:01 -0500

    Added the EventFactory singleton class

---
+++ src/edu/umw/cpsc240fall2015team7/zork/EventFactory.java
@@ -0,0 +1,36 @@
+/**
+  *A singleton class that parses event strings and returns Event objects.
+  *@author Nathanael Woodhead
+  */
+package edu.umw.cpsc240fall2015team7.zork;
+class EventFactory{
+
+	public static EventFactory theInstance;
+	/**
+	  *Creates a new EventFactory object if one does not already exist. If one already exists it will
+	  *return the existing EventFactory object.
+	  *@author Nathanael Woodhead
+	  */
+	public static EventFactory instance(){
+		if(theInstance == null){
+			theInstance = new EventFactory();
+		}
+		return theInstance;
+	}
+	/**
+	  *Empty Constructor.
+	  *@author Nathanael Woodhead
+	  */
+	private EventFactory(){
+	}
+	/**
+	  *Returns the correct Event object for the string entered. 
+	  *@throws NoEventException When no Event matches the string passed.
+	  *@author Nathanael Woodhead
+	  */
+	Event parse(String eventString){
+
+		return null;
+	}
+}
+
