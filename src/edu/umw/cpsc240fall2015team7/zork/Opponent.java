69173d629a8cf84cc59fe41cc6db887f9da173d5
Carson Meadows <cmeadows@rosemary@umw.edu>
2015-11-05 23:41:33 -0500

    Created Opponent.java

---
+++ src/edu/umw/cpsc240fall2015team7/zork/Opponent.java
@@ -0,0 +1,28 @@
+package edu.umw.cpsc240fall2015team7.zork;
+/**
+*
+*@author Carson Meadows
+*/
+class Opponent extends Npc {
+
+        /**
+        *Constructs this Opponent.
+        *@author Carson Meadows
+        */
+        public Opponent (String primaryName, int health, int power, Room room) {
+        }
+
+        /**
+        *Damages the player equal to this Opponent's power.
+        *@author Carson Meadows
+        */
+        public void attackPlayer () {
+        }
+	/**
+	*Moves this Opponent to an adjacent Room.
+	*@author Carson Meadows
+	*/
+	public void move () {
+	}
+}
+
