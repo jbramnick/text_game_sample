8b288eded88e71adf15c4216d93a7f2c7d21ca4f
Nathan Woodhead <nwoodhea@mail.umw.edu>
2015-11-05 20:11:39 -0500

    Added Melee

---
+++ src/edu/umw/cpsc240fall2015team7/zork/Melee.java
@@ -0,0 +1,23 @@
+package edu.umw.cpsc240fall2015team7.zork;
+/**
+  *A close range weapon.
+  *@author Nathanael Woodhead
+  */
+public class Melee extends Item{
+	private int power;
+	/**
+	  *Creates a new Melee object. A Melee weapon is a weapon that hits at close range. This allows opponents to hit back at the
+	  *same time.
+	  *@param power The amount of damage done by the weapon with one hit.
+	  *@param primaryName The name of the Melee weapon object.
+	  *@param weight How much the Melee object weighs.
+	  *@author Nathanael Woodhead
+	  */
+	public Melee(int power,  String primaryName, int weight){
+	}
+	/**
+	  *Returns the power.
+	  *@author Nathanael Woodhead
+	  */
+	public int getPower(){
+	}
