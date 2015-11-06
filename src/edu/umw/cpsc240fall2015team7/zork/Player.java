55b943fb2888d0d8aed7f5c91eef0ecd5c441c16
Nathan Woodhead <nwoodhea@mail.umw.edu>
2015-11-05 23:18:13 -0500

    Added Player

---
+++ src/edu/umw/cpsc240fall2015team7/zork/Player.java
@@ -0,0 +1,23 @@
+package edu.umw.cpsc240fall2015team7.zork;
+/**
+  *The Player of the game.
+  *@author Nathanael Woodhead
+  */
+class Player{
+
+	public static Player theInstance;
+	/**
+	  *If a player does not already exist creates a new player and return it. Otherwise will return the Player object.
+	  *@author Nathanael Woodhead
+	  */
+	public static Player instance(){
+
+	}
+	/**
+	  *A private constructor that creates a player object with the default Values.
+	  *@author Nathanael Woodhead
+	  */
+	private Player(){
+	}
+}
+
