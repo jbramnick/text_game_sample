388c023f5d747ad92b8662b56432440624455827
Carson Meadows <cmeadows@rosemary@umw.edu>
2015-11-05 20:22:43 -0500

    Created EvilGuyWoundEvent.java

--- src/edu/umw/cpsc240fall2015team7/zork/EvilGuyWoundEvent.java
+++ src/edu/umw/cpsc240fall2015team7/zork/EvilGuyWoundEvent.java
@@ -1,24 +1,25 @@
 package edu.umw.cpsc240fall2015team7.zork;
 /**
-*The event that handles the death of the EvilGuy NPC.
+*The event that handles the wounding of the EvilGuy NPC.
 *@author Carson Meadows
 */
-class EvilGuyDieEvent {
+class EvilGuyWoundEvent {
         private EvilGuy evilGuy;
-        
-	/**
-        *Sets evilGuy to be killed.
+
+        /**
+        *Constructs this Event.
         *@author Carson Meadows
         */
         public EvilGuyDieEvent(EvilGuy evilGuy) {
         }
-        
-	/**
-        *Executes the command to kill the evilGuy.
+
+        /**
+        *Executes the command to wound the evilGuy.
         *@author Carson Meadows
         */
         public String execute() {
         }
 
 }
+
 
