1ded009e801032d0cf2a035582c92f4ab3170cfc
Jim <jbramnick412@gmail.com>
2015-11-05 20:34:12 -0500

    added eat event.java

---
+++ src/edu/umw/cpsc240fall2015team7/zork/EatEvent.java
@@ -0,0 +1,24 @@
+package edu.umw.cpsc240fall2015team7.zork;
+/**
+The event that handles eating food to satate hunger.
+@author Jim Bramnick
+*/
+class EatEvent
+{
+	private int amt;
+	/**
+	Creat new EatEvent object and set the amt.
+	@author Jim Bramnick
+	*/
+	public EatEvent(int amt )
+	{
+	}
+	/**
+	Executes the command to add the hunger amt to the hunger in GameState.
+	@author Jim Bramnick
+	*/
+	public String execute()
+	{
+	}
+
+}
