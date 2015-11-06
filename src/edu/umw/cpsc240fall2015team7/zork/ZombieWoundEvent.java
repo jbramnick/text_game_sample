09d7052071581b91be835611d7b459a828edf23f
Carson Meadows <cmeadows@rosemary@umw.edu>
2015-11-05 20:06:13 -0500

    Spell checked ZombieDie- and Wound-Event

--- src/edu/umw/cpsc240fall2015team7/zork/ZombieWoundEvent.java
+++ src/edu/umw/cpsc240fall2015team7/zork/ZombieWoundEvent.java
@@ -1,7 +1,7 @@
 package edu.umw.cpsc240fall2015team7.zork;
 /**
-the event that handles the wounding of a zombie npc.
-@author Jim Bramnick
+*The event that handles the wounding of a zombie NPC.
+*@author Jim Bramnick
 */
 class ZombieWoundEvent
 {
@@ -14,7 +14,7 @@ class ZombieWoundEvent
 	{
 	}
 	/**
-	Executes the command to wound the zombie reducing the zombie's health.
+	Executes the command to reduce the zombie's health.
 	If the zombie's health falls below zero a ZombieDieEvent is called.
 	@author Jim Bramnick
 	*/
