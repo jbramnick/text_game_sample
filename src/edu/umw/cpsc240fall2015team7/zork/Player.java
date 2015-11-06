cac99e0567656fdcca627c954445b2149ba1efec
Nathan Woodhead <nwoodhea@mail.umw.edu>
2015-11-05 23:20:10 -0500

    Fixed Player

--- src/edu/umw/cpsc240fall2015team7/zork/Player.java
+++ src/edu/umw/cpsc240fall2015team7/zork/Player.java
@@ -4,14 +4,13 @@ package edu.umw.cpsc240fall2015team7.zork;
   *@author Nathanael Woodhead
   */
 class Player{
-
+	private int ammo, snacks, medkit;
 	public static Player theInstance;
 	/**
 	  *If a player does not already exist creates a new player and return it. Otherwise will return the Player object.
 	  *@author Nathanael Woodhead
 	  */
 	public static Player instance(){
-
 	}
 	/**
 	  *A private constructor that creates a player object with the default Values.
