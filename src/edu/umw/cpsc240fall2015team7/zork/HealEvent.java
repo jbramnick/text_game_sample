c6bfca5a4a0a8f54dfcb8c5dca13991908f531fa
Jim <jbramnick412@gmail.com>
2015-11-05 21:05:56 -0500

    Added heal even.java

---
+++ src/edu/umw/cpsc240fall2015team7/zork/HealEvent.java
@@ -0,0 +1,25 @@
+package edu.umw.cpsc240fall2015team7.zork;
+/**
+Event used to set health in GameState to MaxHealth.
+@author Jim Bramnick
+*/
+class HealEvent
+{
+	/**
+	Creates a HealEvent Object.
+	@author Jim Bramnick
+	*/
+	public HealEvent()
+	{
+
+	}
+	/**
+	Executes heal Event and sets GameState health to GameState MaxHealth.
+	@author Jim Bramnick
+	*/
+	public String Execute()
+	{
+		return "";
+	}
+
+}
