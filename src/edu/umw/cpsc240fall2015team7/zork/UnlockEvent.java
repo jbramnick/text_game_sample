6c7e10ac42b0ff41ba294d24169f3ed76c03a4d8
Jim <jbramnick412@gmail.com>
2015-11-06 17:30:33 -0500

    Made UnlockEvent.java Prettier

--- src/edu/umw/cpsc240fall2015team7/zork/UnlockEvent.java
+++ src/edu/umw/cpsc240fall2015team7/zork/UnlockEvent.java
@@ -1,6 +1,6 @@
 package edu.umw.cpsc240fall2015team7.zork;
 /**
-The event used to unlock locked exits.
+The event used to unlock locked {@link Exit}s.
 @author Jim Bramnick
 */
 class UnlockEvent
@@ -14,8 +14,8 @@ class UnlockEvent
 
 	}
 	/**
-	Executes the UnlockEvent Object and unlocks all locked exits in the current room.
-	Silently does nothing if there are no locked exits in the room.
+	Executes this and unlocks all locked {@link Exit}s in the current {@link Room}.
+	Silently does nothing if there are no locked {@link Exit}s in the {@link Room}.
 	@author Jim Bramnick
 	*/
 	public String execute()
