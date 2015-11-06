b0480eee7c836225c8d6f21a9121eb6b4049ab0d
Carson Meadows <cmeadows@rosemary@umw.edu>
2015-11-05 20:19:35 -0500

    Added EvilGuyDieEventjava

---
+++ src/edu/umw/cpsc240fall2015team7/zork/EvilGuyDieEvent.java
@@ -0,0 +1,24 @@
+package edu.umw.cpsc240fall2015team7.zork;
+/**
+*The event that handles the death of the EvilGuy NPC.
+*@author Carson Meadows
+*/
+class EvilGuyDieEvent {
+        private EvilGuy evilGuy;
+        
+	/**
+        *Sets evilGuy to be killed.
+        *@author Jim Bramnick
+        */
+        public EvilGuyDieEvent(EvilGuy evilGuy) {
+        }
+        
+	/**
+        *Executes the command to kill the evilGuy.
+        *@author Carson Meadows
+        */
+        public String execute() {
+        }
+
+}
+
