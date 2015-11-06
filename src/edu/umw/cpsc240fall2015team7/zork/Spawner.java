2623f5ce57cc52fcbd7f5cf9083068dd1c71351c
Carson Meadows <cmeadows@rosemary@umw.edu>
2015-11-05 22:33:50 -0500

    Created Spawner.java

---
+++ src/edu/umw/cpsc240fall2015team7/zork/Spawner.java
@@ -0,0 +1,32 @@
+package edu.umw.cpsc240fall2015team7.zork;
+/**
+*Singleton class which creates NPC's to be placed in Rooms in the Dungeon.
+*@author Carson Meadows
+*/
+class Spawner {
+	Dungeon d;
+	
+	/**
+	*Constructs this Spawner.
+	*@author Carson Meadows
+	*/
+        private static Spawner (Dungeon d) {
+		this.d=d;
+	}
+
+        /**
+        *
+        *@author Carson Meadows
+        */
+        public void instance () {
+        }
+
+        /**
+        *.
+        *@author Carson Meadows
+        */
+        public void spawn () {
+        }
+
+}
+
