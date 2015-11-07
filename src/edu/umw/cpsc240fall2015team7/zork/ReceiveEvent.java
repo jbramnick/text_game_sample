f0ad7a373dabc7e20871a1295fa8dd49bb54a89e
Carson Meadows <cmeadows@rosemary@umw.edu>
2015-11-06 19:12:22 -0500

    Sanity checked up to Player.java

--- src/edu/umw/cpsc240fall2015team7/zork/ReceiveEvent.java
+++ src/edu/umw/cpsc240fall2015team7/zork/ReceiveEvent.java
@@ -7,14 +7,14 @@ class ReceiveEvent extends Event{
 	private Item item;
 	private int medkits,ammo,snacks;
 	/**
-	Creates a new ReceiveEvent.
+	Constructs this ReceiveEvent.
 	@author Jim Bramnick
 	*/
 	public ReceiveEvent(Item item, int medkits, int snacks, int ammo){
 
 	}
 	/**
-	Adds the item to the {@link Player} Inventory and adds medkits,ammo and snacks to {@link Player} medkits,ammo,snacks. 
+	Adds the item to the {@link Player} Inventory and adds medkits, ammo and snacks to {@link Player} medkits, ammo and snacks. 
 	If item already exists in the Inventory of {@link Player} still adds that item.
 	If item is null silently does nothing.
 	@author Jim Bramnick
