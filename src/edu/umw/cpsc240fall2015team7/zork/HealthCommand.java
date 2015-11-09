399dc9406a13321ea6a0ffc86ab90a81fde78cfe
Nathan Woodhead <nwoodhea@mail.umw.edu>
2015-11-09 15:01:46 -0500

    Nathan added the health command.

---
+++ src/edu/umw/cpsc240fall2015team7/zork/HealthCommand.java
@@ -0,0 +1,11 @@
+package edu.umw.cpsc240fall2015team7.zork;
+class HealthCommand extends Command{
+	HealthCommand(){}
+
+	String execute(){
+		String text = "";
+		int health = Player.instance().getHealth();
+		if(health == 100){
+			text = "You couldn't feel better!";
+		}	
+		return text;
