a8a07bc1210adfd685a9b539a9c20ddad933cd5d
Jim Bramnick <jbramnick412@gmail.com>
2015-11-12 19:18:19 -0500

    Reduced errors in command factory

--- src/edu/umw/cpsc240fall2015team7/zork/EventFactory.java
+++ src/edu/umw/cpsc240fall2015team7/zork/EventFactory.java
@@ -1,4 +1,6 @@
 package edu.umw.cpsc240fall2015team7.zork;
+import java.util.ArrayList;
+import java.lang.reflect.Constructor;
 /**
   *A singleton class that parses event strings and returns {@link Event} objects.
   *@author Nathanael Woodhead
@@ -24,13 +26,36 @@ class EventFactory{
 	private EventFactory(){
 	}
 	/**
-	  *Returns the correct Event object for the String entered. 
-	  *@throws NoEventException If no Event matches the String passed.
-	  *@author Nathanael Woodhead
-	  */
-	Event parse(String eventString){
-
+	 *Returns the correct Event object for the String entered. 
+	 *@throws NoEventException If no Event matches the String passed.
+	 *@author Nathanael Woodhead
+	 */
+	ArrayList<Event> parse(Item item,String eventString) throws Dungeon.IllegalDungeonFormatException
+	{
+		String[] eventsStrings=eventString.split(",");
+		ArrayList<String> parenEvents=new ArrayList<String>();
+		ArrayList<String> normalEvents=new ArrayList<String>();
+		ArrayList<Event> events=new ArrayList<Event>();
+		for(int i=0;i<eventsStrings.length;i++)
+		{
+			if(eventsStrings[i].contains("("))
+			{
+				parenEvents.add(eventsStrings[i]);
+			}
+			else
+				normalEvents.add(eventsStrings[i]);
+		}
+		for(int i=0;i<parenEvents.size();i++)
+		{
+			String event=parenEvents.get(i).substring(0,parenEvents.get(i).indexOf("("));
+			event = "edu.umw.cpsc240fall2015team7.zork."+event+"Event";
+			Class clazz=Class.forName(event);
+			Constructor cons=clazz.getDeclaredConstructor(Item.class,String.class);
+			Event theEvent=(Event)cons.newInstance(item,parenEvents.get(i).substring(parenEvents.get(i).indexOf("(")+1,parenEvents.get(i).indexOf(")")));
+			events.add(theEvent);
+		}
 		return null;
+		
 	}
 }
 
