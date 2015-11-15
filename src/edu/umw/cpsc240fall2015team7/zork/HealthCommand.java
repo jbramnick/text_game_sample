73579842b7ef9a144beb426e59968ee5c8fad901
Carson Meadows <cmeadows@rosemary@umw.edu>
2015-11-15 15:22:05 -0500

    Gave variables to Weapon.java and fixed HealthCommand issue

--- src/edu/umw/cpsc240fall2015team7/zork/HealthCommand.java
+++ src/edu/umw/cpsc240fall2015team7/zork/HealthCommand.java
@@ -1,4 +1,8 @@
 package edu.umw.cpsc240fall2015team7.zork;
+/**
+*Returns a message correlating to the Player's health.
+*@author Carson Meadows
+*/
 class HealthCommand extends Command{
 	HealthCommand(){}
 
@@ -7,7 +11,15 @@ class HealthCommand extends Command{
 		int health = Player.instance().getHealth();
 		if(health == 100){
 			text = "You couldn't feel better!";
-		}	
+		} else if (health > 75) {
+			text = "You feel fine, but not perfect.";
+		} else if (health > 50) {
+			text = "You're hurt, but your strong will carries you on!";
+		} else if (health > 25) {
+			text = "Each step is a painful struggle. You need to heal!";
+		} else {
+			text = "You are inches from death.";
+		}
 		return text + "\n";
 	}
 }
