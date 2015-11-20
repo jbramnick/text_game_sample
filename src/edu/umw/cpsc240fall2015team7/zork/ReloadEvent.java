95b3a9a49f45f29eb8fb4542f01c664fde37f40e
Nathan Woodhead <nwoodhea@mail.umw.edu>
2015-11-19 21:52:28 -0500

    added ReloadEvent

---
+++ src/edu/umw/cpsc240fall2015team7/zork/ReloadEvent.java
@@ -0,0 +1,50 @@
+package edu.umw.cpsc240fall2015team7.zork;
+/**
+  *A Event for reloading a gun.
+  *@author Nathanael Woodhead
+  */
+class ReloadEvent extends Event{
+	private Gun weapon;
+	private String commandString;
+	/**
+	  *Creates a new ReloadEvent object.
+	  *@author Nathanael Woodhead
+	  */
+	ReloadEvent(Item item, String commandString){
+		this.commandString = commandString;
+	}
+	/**
+	  *Parses the commandString and carries out the necessary actions. 
+	  *The commandString should be in the format "reload gun". This should parse the commandString and extract the gun's name. 
+	  *If there is no gun by that name in the players inventory then this should return a message saying that you do not have that 
+	  *gun. If the gun is in the player inventory this will reload it and return conformation message.
+	  *@author Nathanael Woodhead
+	  */
+	String execute(){
+		String weaponString = commandString.substring(7,commandString.length()+1);
+		this.weapon = Player.instance().getItemInInventoryNamed(weaponString);
+		if(weapon == null){
+			return "You don't have a " + weaponString + ".";
+		}
+		int capacity = weapon.getCapacity();
+		int currAmmo = weapon.getAmmo();
+		String ammoType = weapon.getAmmoType();
+		int supply = Player.instance().countAmmo(ammoType);
+		int toAdd = capacity - currAmmo;
+		if (supply >= toAdd) {
+			weapon.reload(toAdd);
+			while(toAdd>0){
+				Player.instace.removeItem(ammoType);
+				toAdd = toAdd - 1;
+			}
+		}else{
+			weapon.reload(supply);
+			while(toAdd>0){
+				Player.instace().removeItem(ammoType);
+				toAdd = toAdd -1;
+			}
+		}
+		return "Reloaded " + weaponString+".";	
+	}
+}
+	
