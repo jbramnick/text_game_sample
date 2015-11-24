44984f4754b9f99d8d7e001ae55cbee91c3c6737
Jim Bramnick <jbramnick412@gmail.com>
2015-11-24 14:31:16 -0500

    Implemented barebones help command

---
+++ src/edu/umw/cpsc240fall2015team7/zork/HelpCommand.java
@@ -0,0 +1,17 @@
+package edu.umw.cpsc240fall2015team7.zork;
+class HelpCommand extends Command{
+	private String commandString;
+	/**
+	  *Creates a new HelpCommand object.
+	  *@author Nathanael Woodhead
+	  */
+	HelpCommand(){}
+	/**
+	  *Checks the Player's current score and returns a message containing it. 
+	  *@author Nathanael Woodhead
+	  */
+	String execute(){
+		String text = "";
+		return text + "\n";
+	}
+}
